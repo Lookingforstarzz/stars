550531d513d50d58096e91028bb5dc401ffbcb90
Lookingforstarzz <48636732+Lookingforstarzz@users.noreply.github.com>
2019-04-14 11:47:33 +0800

    Add files via upload

---
+++ turtle_design.py
@@ -0,0 +1,33 @@
+import turtle
+
+def draw_squares(some_squares):
+    some_squares.left(90)
+    some_squares.forward(250)
+    for i in range(36):
+         some_squares.forward(100)
+         some_squares.right(90)
+         some_squares.forward(100)
+         some_squares.right(90)
+         some_squares.forward(100)
+         some_squares.right(90)
+         some_squares.forward(100)
+         some_squares.right(90)
+         some_squares.right(10)
+
+         
+
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("red")
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("aqua")
+    brad.speed(10)
+    draw_squares(brad)
+
+
+    window.exitonclick()
+
+
+draw_art()
